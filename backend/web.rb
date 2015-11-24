5e5166f09581a3f8034e4102cf7db44d52236141
Daniel Naranjo <dnaranjo@gmail.com>
2015-11-24 16:14:20 -0300

    gitignore, php, ruby

---
+++ backend/web.rb
@@ -0,0 +1,36 @@
+#web.rb
+require 'sinatra'
+require 'omniauth-twitter'
+
+use OmniAuth::Builder do
+	provider :twitter, 'key','secret'
+end
+
+configure do
+	enable :sessions
+end
+
+helpers do
+	def admin?
+		session[:admin]
+	end
+end
+
+get '/' do
+  "<h3>Hey! Hey! Welcome to Trento public API</h3><p>Trento is a demo assigment for <a href=\"//real-trends.com/?utm_source=trento&utm_campaing=assigments&utm_medium=referral\" target=\"_blank\">Real Trends</a></p><p>Crafted by <a href=\"//danielnaranjo.info/?utm_source=trento&utm_campaing=assigments&utm_medium=referral\" target=\"_blank\">Daniel Naranjo</a></p>"
+end
+
+get '/private' do
+	halt(401,'Not Authorized') unless admin?
+	"Private party! Please check in lobby to get in."
+end
+
+get '/login' do
+	session[:admin] = true
+	"You're Logged"
+end
+
+get '/logout' do
+	session[:admin] = nil
+	"You're Out!"
+end
