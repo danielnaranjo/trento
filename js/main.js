9651a5a73a3b557e65a50c220a72fd082dc3239b
Daniel Naranjo <dnaranjo@gmail.com>
2015-11-24 13:48:32 -0300

    Changes UI, added moment.js, JSON mockup

--- js/main.js
+++ js/main.js
@@ -33,6 +33,9 @@ $(function(){
 		// Funny thing is allowed ;)
 		$('#hireMe').attr('href','mailto:daniel@loultimoenlaweb.com?subject=Trento is OK&body=Nice one!');
 
+		// for test propose
+		loadAgain();
+
 	});
 
 	$('textarea').on('keyup', function(){
@@ -48,10 +51,21 @@ $(function(){
 	});
 
 	// for test propose
-	$('#listed ul').html('');
-	$.getJSON('js/sample.json', function(data){
-		$('#listed ul').append('<li>Cargada!</li>');
-		console.log('data', data);
-		$('#listed ul').append('<li>Hola</li>');
-	});
+	function loadAgain() {
+		$.getJSON('js/sample2.json', function(data){
+			$('#listed ul').html('');
+			$.each(data, function(i, field){
+				// Da Tweet
+				$("#listed ul").append('<li id="'+data[i].id+'"><div class="col-md-2"><img src="'+data[i].img+'" alt="avatar" class="img-rounded img-responsive"></div><div class="col-md-9"><h2>'+data[i].title+'</h2><p>'+moment(data[i].date).toNow()+' by <a href="//www.twitter.com/'+data[i].username+'">'+data[i].username+'</a></p><p id="options'+data[i].id+'"></p></div></li>');
+				// Reply :)
+				$('#options'+data[i].id).append('<a href="#'+data[i].id+'" data-toggle="tooltip" data-placement="bottom" title="Reply to this guy!"><i class="fa fa-reply"></i></a> ');
+				// Like
+				$('#options'+data[i].id).append('<a href="#'+data[i].id+'" data-toggle="tooltip" data-placement="bottom" title="Loves... Likes... Favorites... All verbs means the same"><i class="fa fa-heart"></i></a> ');
+				// Delete
+				$('#options'+data[i].id).append('<a href="#" data-toggle="tooltip" data-placement="bottom" title="oh boy! delete! delete! delete!"><i class="fa fa-trash"></i></a> ');
+				// More
+				$('#options'+data[i].id).append('<a href="#'+data[i].id+'" data-toggle="tooltip" data-placement="bottom" title="More option will be great!"><i class="fa fa-ellipsis-h"></i></a> ');
+			});
+		});
+	};
 });
