137857f8521a8a82cf126baf647583b584230638
Daniel Naranjo <dnaranjo@gmail.com>
2015-11-26 17:40:27 -0300

    JS, HTML, CSS changes

--- js/main.js
+++ js/main.js
@@ -17,8 +17,6 @@ function countChar(val) {
 $(function(){
 	// for test propose
 	console.log('Got it!');
-	// Fire tooltip widget!
-	//$('[data-toggle="tooltip"]').tooltip();
 
 	//hide until log in
 	$('aside').css('display','none');
@@ -54,28 +52,44 @@ $(function(){
 	function loadAgain() {
 		$.getJSON('http://localhost:8890/trento/api/me', function(data){ //js/sample2.json
 			$('#listed ul').html('');
-			$('footer').removeClass('position');
+			$('footer').addClass('animated bounceOutDown');
+
+			$(window).scroll(function() {
+				if($(this).scrollTop()>0) {
+					$('aside').addClass('animated fadeOutLeft');
+				// } else {
+					// $('aside').addClass('animated fadeInLeft');
+				}
+				if($(this).scrollTop()<10) {
+					$('aside').removeClass('fadeOutLeft').addClass('bounceInLeft');
+				}
+			});
 
 
 			$.each(data, function(i, field){
 				$('#name').html(data[i].user.name);
 				$('#nickname').html(data[i].user.screen_name);
 				$('#description').html(data[i].user.description);
-				$('#url').html('Check it out at '+data[i].user.url);
+				$('#url').html('Check it out at <a href="'+data[i].user.url+'">'+data[i].user.url+'</a>');
 				$('#join').html('Playing here since '+moment(data[i].user.created_at).toNow());
 
 				// Da Tweet
-				$("#listed ul").append('<li id="'+data[i].id+'"><div class="col-md-2"><img src="'+data[i].user.profile_image_url+'" alt="avatar" class="img-rounded img-responsive"></div><div class="col-md-9"><h2>'+data[i].text+'</h2><p>'+moment(data[i].created_at).toNow()+' by <a href="javascript:;" data-toggle="modal" data-target="#myModal" >'+data[i].user.screen_name+'</a></p><p id="options'+data[i].id+'"></p></div></li>').addClass('animated bounceInUp');
+				$("#listed ul").append('<li id="'+data[i].id+'"><div class="col-md-2"><img src="'+data[i].user.profile_image_url+'" alt="avatar" class="img-rounded img-responsive"></div><div class="col-md-9"><h2>'+data[i].text+'</h2><p>'+moment(data[i].created_at).toNow()+' by <a target="_blank" href="//twitter.com/'+data[i].user.screen_name+'" data-toggle="modal" data-target="#myModal">'+data[i].user.screen_name+'</a></p><p id="options'+data[i].id+'"></p></div></li>').addClass('animated bounceInUp');
 				
 				// Reply :)
 				$('#options'+data[i].id).append('<a href="/replyTo/'+data[i].id+'" data-toggle="tooltip" data-placement="bottom" title="Reply to this guy!"><i class="fa fa-reply"></i></a> '+ data[i].retweet_count+' ');
 				// Like
-				$('#options'+data[i].id).append('<a href="/api/loveIt/'+data[i].id+'" data-toggle="tooltip" data-placement="bottom" title="Loves... Likes... Favorites... All verbs means the same"><i class="fa fa-heart"></i></a> '+ data[i].favorite_count+'');
+				$('#options'+data[i].id).append('<a href="/api/loveIt/'+data[i].id+'" title="Loves... Likes... Favorites... All verbs means the same"><i class="fa fa-heart"></i></a> '+ data[i].favorite_count+'');
 				// Delete
-				$('#options'+data[i].id).append('<a href="/api/delete/'+data[i].id+'" data-toggle="tooltip" data-placement="bottom" title="oh boy! delete! delete! delete!"><i class="fa fa-trash"></i></a> ');
+				$('#options'+data[i].id).append('<a href="/api/delete/'+data[i].id+'" title="oh boy! delete! delete! delete!"><i class="fa fa-trash"></i></a> ');
 				// More
-				$('#options'+data[i].id).append('<a href="api/more/'+data[i].id+'" data-toggle="tooltip" data-placement="bottom" title="More option will be great!"><i class="fa fa-ellipsis-h"></i></a> ');
+				$('#options'+data[i].id).append('<a href="/api/more/'+data[i].id+'" title="More option will be great!"><i class="fa fa-ellipsis-h"></i></a> ');
+
+				$('#options'+data[i].id).attr('data-toggle="tooltip" data-placement="bottom"');
 			});
+
 		});
-	};
+	}
+	// Fire tooltip widget!
+	$('[data-toggle="tooltip"]').tooltip();
 });
