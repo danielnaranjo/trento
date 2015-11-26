891a8fab0f771088faac20bf012a20c8f97f1c2c
Daniel Naranjo <dnaranjo@gmail.com>
2015-11-26 16:50:53 -0300

    Changes and improvements

--- js/main.js
+++ js/main.js
@@ -52,20 +52,29 @@ $(function(){
 
 	// for test propose
 	function loadAgain() {
-		$.getJSON('js/sample2.json', function(data){
+		$.getJSON('http://localhost:8890/trento/api/me', function(data){ //js/sample2.json
 			$('#listed ul').html('');
+			$('footer').removeClass('position');
+
+
 			$.each(data, function(i, field){
+				$('#name').html(data[i].user.name);
+				$('#nickname').html(data[i].user.screen_name);
+				$('#description').html(data[i].user.description);
+				$('#url').html('Check it out at '+data[i].user.url);
+				$('#join').html('Playing here since '+moment(data[i].user.created_at).toNow());
+
 				// Da Tweet
-				$("#listed ul").append('<li id="'+data[i].id+'"><div class="col-md-2"><img src="'+data[i].img+'" alt="avatar" class="img-rounded img-responsive"></div><div class="col-md-9"><h2>'+data[i].title+'</h2><p>'+moment(data[i].date).toNow()+' by <a href="javascript:;" data-toggle="modal" data-target="#myModal" >'+data[i].username+'</a></p><p id="options'+data[i].id+'"></p></div></li>');//="//www.twitter.com/'+data[i].username+'"
+				$("#listed ul").append('<li id="'+data[i].id+'"><div class="col-md-2"><img src="'+data[i].user.profile_image_url+'" alt="avatar" class="img-rounded img-responsive"></div><div class="col-md-9"><h2>'+data[i].text+'</h2><p>'+moment(data[i].created_at).toNow()+' by <a href="javascript:;" data-toggle="modal" data-target="#myModal" >'+data[i].user.screen_name+'</a></p><p id="options'+data[i].id+'"></p></div></li>').addClass('animated bounceInUp');
 				
 				// Reply :)
-				$('#options'+data[i].id).append('<a href="#'+data[i].id+'" data-toggle="tooltip" data-placement="bottom" title="Reply to this guy!"><i class="fa fa-reply"></i></a> ');
+				$('#options'+data[i].id).append('<a href="/replyTo/'+data[i].id+'" data-toggle="tooltip" data-placement="bottom" title="Reply to this guy!"><i class="fa fa-reply"></i></a> '+ data[i].retweet_count+' ');
 				// Like
-				$('#options'+data[i].id).append('<a href="#'+data[i].id+'" data-toggle="tooltip" data-placement="bottom" title="Loves... Likes... Favorites... All verbs means the same"><i class="fa fa-heart"></i></a> ');
+				$('#options'+data[i].id).append('<a href="/api/loveIt/'+data[i].id+'" data-toggle="tooltip" data-placement="bottom" title="Loves... Likes... Favorites... All verbs means the same"><i class="fa fa-heart"></i></a> '+ data[i].favorite_count+'');
 				// Delete
-				$('#options'+data[i].id).append('<a href="#" data-toggle="tooltip" data-placement="bottom" title="oh boy! delete! delete! delete!"><i class="fa fa-trash"></i></a> ');
+				$('#options'+data[i].id).append('<a href="/api/delete/'+data[i].id+'" data-toggle="tooltip" data-placement="bottom" title="oh boy! delete! delete! delete!"><i class="fa fa-trash"></i></a> ');
 				// More
-				$('#options'+data[i].id).append('<a href="#'+data[i].id+'" data-toggle="tooltip" data-placement="bottom" title="More option will be great!"><i class="fa fa-ellipsis-h"></i></a> ');
+				$('#options'+data[i].id).append('<a href="api/more/'+data[i].id+'" data-toggle="tooltip" data-placement="bottom" title="More option will be great!"><i class="fa fa-ellipsis-h"></i></a> ');
 			});
 		});
 	};
