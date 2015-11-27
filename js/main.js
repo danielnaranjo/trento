3868c63d505434d322e46248b7613e50b2b9345b
Daniel Naranjo <dnaranjo@gmail.com>
2015-11-26 22:27:39 -0300

    Changes

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ $(function(){
 		$('#hireMe').attr('href','mailto:daniel@loultimoenlaweb.com?subject=Trento is OK&body=Nice one!');
 
 		// for test propose
-		loadAgain();
+		getMore();
 
 	});
 
@@ -48,48 +48,54 @@ $(function(){
 		}
 	});
 
+	$(window).scroll(function() {
+		if($(this).scrollTop()>0) {
+			$('aside').css('height','2000px').addClass('animated fadeOutLeft');
+		// } else {
+			// $('aside').addClass('animated fadeInLeft');
+		}
+		if($(this).scrollTop()<10) {
+			$('aside').removeClass('fadeOutLeft').addClass('bounceInLeft');
+		}
+	});
+
+
 	// for test propose
-	function loadAgain() {
+	function getMore() {
 		$.getJSON('http://localhost:8890/trento/api/me', function(data){ //js/sample2.json
 			$('#listed ul').html('');
+			$('#more').css('display','inline').addClass('animated fadeIn');
 			$('footer').addClass('animated bounceOutDown');
 
-			$(window).scroll(function() {
-				if($(this).scrollTop()>0) {
-					$('aside').addClass('animated fadeOutLeft');
-				// } else {
-					// $('aside').addClass('animated fadeInLeft');
-				}
-				if($(this).scrollTop()<10) {
-					$('aside').removeClass('fadeOutLeft').addClass('bounceInLeft');
-				}
-			});
-
-
 			$.each(data, function(i, field){
-				$('#name').html(data[i].user.name);
-				$('#nickname').html(data[i].user.screen_name);
+				$('#name').html('<a href="//www.twitter.com/'+data[i].user.screen_name+'">'+data[i].user.name+'</a>');
 				$('#description').html(data[i].user.description);
 				$('#url').html('Check it out at <a href="'+data[i].user.url+'">'+data[i].user.url+'</a>');
 				$('#join').html('Playing here since '+moment(data[i].user.created_at).toNow());
 
 				// Da Tweet
-				$("#listed ul").append('<li id="'+data[i].id+'"><div class="col-md-2"><img src="'+data[i].user.profile_image_url+'" alt="avatar" class="img-rounded img-responsive"></div><div class="col-md-9"><h2>'+data[i].text+'</h2><p>'+moment(data[i].created_at).toNow()+' by <a target="_blank" href="//twitter.com/'+data[i].user.screen_name+'" data-toggle="modal" data-target="#myModal">'+data[i].user.screen_name+'</a></p><p id="options'+data[i].id+'"></p></div></li>').addClass('animated bounceInUp');
+				$("#listed ul").append('<li id="'+data[i].id+'" class="box"><div class="col-md-2"><img src="'+data[i].user.profile_image_url+'" alt="avatar" class="img-rounded img-responsive"></div><div class="col-md-9"><h2>'+data[i].text+'</h2><p><span id="options'+data[i].id+'"></span> '+moment(data[i].created_at).toNow()+' by <a target="_blank" href="//twitter.com/'+data[i].user.screen_name+'">'+data[i].user.screen_name+'</a></p></div></li>').addClass('animated bounceInUp');
 				
 				// Reply :)
-				$('#options'+data[i].id).append('<a href="/replyTo/'+data[i].id+'" data-toggle="tooltip" data-placement="bottom" title="Reply to this guy!"><i class="fa fa-reply"></i></a> '+ data[i].retweet_count+' ');
+				$('#options'+data[i].id).append('<a href="/replyTo/'+data[i].id+'" data-toggle="tooltip" data-placement="bottom" title="Reply to this guy!"><i class="fa fa-reply"></i></a>');//'+ data[i].retweet_count+'
 				// Like
-				$('#options'+data[i].id).append('<a href="/api/loveIt/'+data[i].id+'" title="Loves... Likes... Favorites... All verbs means the same"><i class="fa fa-heart"></i></a> '+ data[i].favorite_count+'');
+				$('#options'+data[i].id).append('<a href="/api/loveIt/'+data[i].id+'" title="Loves... Likes... Favorites... All verbs means the same"><i class="fa fa-heart"></i></a>');//'+ data[i].favorite_count+'
 				// Delete
-				$('#options'+data[i].id).append('<a href="/api/delete/'+data[i].id+'" title="oh boy! delete! delete! delete!"><i class="fa fa-trash"></i></a> ');
+				$('#options'+data[i].id).append('<a href="/api/delete/'+data[i].id+'" title="oh boy! delete! delete! delete!"><i class="fa fa-trash"></i></a>');
 				// More
-				$('#options'+data[i].id).append('<a href="/api/more/'+data[i].id+'" title="More option will be great!"><i class="fa fa-ellipsis-h"></i></a> ');
+				$('#options'+data[i].id).append('<a href="javascript:;" data-toggle="modal" data-target="#myModal" title="More option will be great!"><i class="fa fa-ellipsis-h"></i></a>');
 
 				$('#options'+data[i].id).attr('data-toggle="tooltip" data-placement="bottom"');
 			});
-
 		});
 	}
+
+	// refresh()
+	$('#getMore').on('click', function(){
+		$('#listed ul').html('');
+		getMore();
+		console.log('getMore clicked');
+	});
 	// Fire tooltip widget!
 	$('[data-toggle="tooltip"]').tooltip();
 });
